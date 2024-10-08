5ef6ea19be31e97d3d680680697335e4989a4e92
MalvaTalpa <ciaosara15@yahoo.com>
2024-10-08 14:23:12 +0200

    min modifications

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import paramiko
 from fabric import Connection
 import threading
 
-#caso 0 ->  Is when we have a test with nothing running on top of it.
+
+# caso 0 ->  Is when we have a test with nothing running on top of it.
 #           I take the counters at the beginning and at the end of test
-#case 1 ->  Is when we have a test with up and down scaling for multiple times.
+# case 1 ->  Is when we have a test with up and down scaling for multiple times.
 #           I take counter before the test, when the test is running every 10 minutes, and at the end
 
 # ssh = paramiko.SSHClient()
@@ -18,16 +19,23 @@ def run_scale_up_down():
     with c.cd('/home/ubuntu/pktgen'):
         result1 = c.run('./client_exe client.yaml /home/ubuntu/.kube/config')
 
-def energy_counter(energy_counter):
-    time.sleep(600)
+def get_energy():
     r = requests.get(URL + "/api/CPUGovernor/" + energy_name + "?hostName=" + hostname)
-    energy_counter.append(r)
+    x = r.text
+    print(x)
+    energy_counter.append(r.text)
+
+def get_counters():
+    for idx, i in enumerate(counters_name):
+        r = requests.get(URL + "/api/CPUGovernor/" + counters_name[idx] + "?hostName=" + hostname)
+        start_counter.append(r.text)
+
 
 
 test_type = 0
 timer = 0
 URL = "http://192.168.13.5:8001"
-hostname = "r2hpgen8-test.maas" #need to add the
+hostname = "r2hpgen8-test.maas"  # need to add the
 # other one
 counters_name = ["Cstates-Usage", "Cstates-Time"]
 energy_name = "EnergyRapl"
@@ -36,22 +44,17 @@ energy_counter = []
 endCounter = []
 
 c = Connection(host="ubuntu@192.168.17.28", connect_kwargs={"password": "pap3rin0"})
-#take the initial couters of CPU usage and Time
-for idx, i in enumerate(counters_name):
-    r = requests.get(URL + "/api/CPUGovernor/" + counters_name[idx] + "?hostName=" + hostname)
-    start_counter.append(r)
+# take the initial counters of CPU usage and Time
+get_counters()
 
-#take the energies couters of CPU usage and Time
+# take the energies counters of CPU usage and Time
 match test_type:
     case 0:
-        r = requests.get(URL + "/api/CPUGovernor/" + energy_name + "?hostName=" + hostname)
-        energy_counter.append(r)
+        print("Case no scaling is going on")
+        get_energy()
         print("start_sleep")
-        while timer < 12:
-            time.sleep(600)
-            r = requests.get(URL + "/api/CPUGovernor/" + energy_name + "?hostName=" + hostname)
-            energy_counter.append(r)
-            timer += 1
+        #time.sleep(43200)
+
     case 1:
         r = requests.get(URL + "/api/CPUGovernor/" + energy_name + "?hostName=" + hostname)
         energy_counter.append(r)
@@ -61,3 +64,7 @@ match test_type:
             r = requests.get(URL + "/api/CPUGovernor/" + energy_name + "?hostName=" + hostname)
             energy_counter.append(r)
             timer += 1
+
+get_counters()
+get_energy()
+min
